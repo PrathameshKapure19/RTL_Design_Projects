36f69897825e4e5d84f077edec3c67bb747a9214
Prathamesh Yogesh Kapure <prathameshkapure19@gmail.com>
2026-03-03 13:33:52 +0530

    3 Bit Multiplier Design and Verfication

--- Multiplier/3 Bit Multiplier Design and Functional Verfication/multipler_3bit.v
+++ Multiplier/3 Bit Multiplier Design and Functional Verfication/multipler_3bit.v
@@ -5,14 +5,14 @@ output [5:0]P
     );
  wire w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12;
  assign P[0] = A[0] & B[0];
- 
+  
  assign w1   = A[1] & B[0];
  assign w2   = A[0] & B[1];
  half_adder  HA1 (w1,w2,P[1],w3);
  
  assign w4   = A[2] & B[0];
  assign w5   = A[1] & B[1];
- full_adder FA1(w3,w4,w5,w6,w7);
+ full_adder FA1(w3,w4,w5,w6,w7); 
  
  assign w8   = A[2] & B[1];
  half_adder  HA2(w7,w8,w10,w9);
